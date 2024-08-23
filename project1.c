054f718c1fe3b355657295266cc67ecc8a6bc07e
soumenmaity3 <sm8039912@gmail.com>
2024-08-23 22:10:08 +0530

    Update this game

--- project1.c
+++ project1.c
@@ -2,30 +2,53 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+
+int show(){
+  printf("This is a number choise game.\n");
+    printf("You have 10 chance to complete this game. Lets play....");
+}
+int game() {
     srand(time(0));
     int random;
     int randomNumber = rand();
     int count=0;
+    int chance=10;
+    char ch;
     int randomInRange = rand() % 100+1;
-    printf("This is a number choise game. Lets play.....\n");
-    do{
+    while(count<10){
       printf("\nEnter a number (0 to 100):- ");
       scanf("%d",&random);
       if(randomInRange> random){
         printf("Enter higher number.\n");
         count++;
-        printf("Your are faild (%d)\n",count);
+        printf("Your are faild (%d/%d)\n",count,chance);
       }else if(randomInRange<random){
         printf("Enter lower number.\n");
         count++;
-        printf("Your are faild (%d)\n",count);
+        printf("Your are faild (%d/%d)\n",count,chance);
       }
       else{
         printf("Congrats.....\n");
       }
-    }while (randomInRange!=random);
-    
+      if(count == 10){
+        printf("Game Over. Right number is- %d",randomInRange);
+        printf("\nThank you for playing.");
+      }
+    }
+}
 
-    return 0;
+int main(){
+  show();
+  game();
+  while(1){
+    char ch;
+    printf("\n\n\nPlay again?(y/n)- ");
+    scanf(" %c",&ch);
+    if(ch=='y'){
+      game();
+    }else{
+      exit(0);
+    }
+  }
+  return 0;
 }
